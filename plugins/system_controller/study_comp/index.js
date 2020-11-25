13232ded9dea301ce2440a7cd085d4326c0959a2
Naufal Kurniawan <naufal.kurniawan.indonesia.111@gmail.com>
2020-11-25 07:51:13 +0700

    test 1.1

--- plugins/system_controller/study_comp/index.js
+++ plugins/system_controller/study_comp/index.js
@@ -275,152 +275,152 @@ studyComp.prototype.shutdown = function() {
 // If your plugin is not a music_sevice don't use this part and delete it
 
 
-studyComp.prototype.addToBrowseSources = function () {
+//studyComp.prototype.addToBrowseSources = function () {
 
 	// Use this function to add your music service plugin to music sources
     //var data = {name: 'Spotify', uri: 'spotify',plugin_type:'music_service',plugin_name:'spop'};
-    this.commandRouter.volumioAddToBrowseSources(data);
-};
+//    this.commandRouter.volumioAddToBrowseSources(data);
+//};
 
-studyComp.prototype.handleBrowseUri = function (curUri) {
-    var self = this;
+//studyComp.prototype.handleBrowseUri = function (curUri) {
+//    var self = this;
 
     //self.commandRouter.logger.info(curUri);
-    var response;
+//    var response;
 
 
-    return response;
-};
+//    return response;
+//};
 
 
 
 // Define a method to clear, add, and play an array of tracks
-studyComp.prototype.clearAddPlayTrack = function(track) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::clearAddPlayTrack');
+//studyComp.prototype.clearAddPlayTrack = function(track) {
+//	var self = this;
+//	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::clearAddPlayTrack');
 
-	self.commandRouter.logger.info(JSON.stringify(track));
+//	self.commandRouter.logger.info(JSON.stringify(track));
 
-	return self.sendSpopCommand('uplay', [track.uri]);
-};
+//	return self.sendSpopCommand('uplay', [track.uri]);
+//};
 
-studyComp.prototype.seek = function (timepos) {
-    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::seek to ' + timepos);
+//studyComp.prototype.seek = function (timepos) {
+//    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::seek to ' + timepos);
 
-    return this.sendSpopCommand('seek '+timepos, []);
-};
+//    return this.sendSpopCommand('seek '+timepos, []);
+//};
 
 // Stop
-studyComp.prototype.stop = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::stop');
+//studyComp.prototype.stop = function() {
+//	var self = this;
+//	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::stop');
 
 
-};
+//};
 
 // Spop pause
-studyComp.prototype.pause = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::pause');
+//studyComp.prototype.pause = function() {
+//	var self = this;
+//	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::pause');
 
 
-};
+//};
 
 // Get state
-studyComp.prototype.getState = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::getState');
+//studyComp.prototype.getState = function() {
+//	var self = this;
+//	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::getState');
 
 
-};
+//};
 
 //Parse state
-studyComp.prototype.parseState = function(sState) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::parseState');
+//studyComp.prototype.parseState = function(sState) {
+//	var self = this;
+//	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::parseState');
 
 	//Use this method to parse the state and eventually send it with the following function
-};
+//};
 
 // Announce updated State
-studyComp.prototype.pushState = function(state) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::pushState');
+//studyComp.prototype.pushState = function(state) {
+//	var self = this;
+//	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'studyComp::pushState');
 
-	return self.commandRouter.servicePushState(state, self.servicename);
-};
+//	return self.commandRouter.servicePushState(state, self.servicename);
+//};
 
 
-studyComp.prototype.explodeUri = function(uri) {
-	var self = this;
-	var defer=libQ.defer();
+//studyComp.prototype.explodeUri = function(uri) {
+//	var self = this;
+//	var defer=libQ.defer();
 
 	// Mandatory: retrieve all info for a given URI
 
-	return defer.promise;
-};
+//	return defer.promise;
+//};
 
-studyComp.prototype.getAlbumArt = function (data, path) {
+//studyComp.prototype.getAlbumArt = function (data, path) {
 
-	var artist, album;
+//	var artist, album;
 
-	if (data != undefined && data.path != undefined) {
-		path = data.path;
-	}
+//	if (data != undefined && data.path != undefined) {
+//		path = data.path;
+//	}
 
-	var web;
+//	var web;
 
-	if (data != undefined && data.artist != undefined) {
-		artist = data.artist;
-		if (data.album != undefined)
-			album = data.album;
-		else album = data.artist;
+//	if (data != undefined && data.artist != undefined) {
+//		artist = data.artist;
+//		if (data.album != undefined)
+//			album = data.album;
+//		else album = data.artist;
 
-		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
-	}
+//		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
+//	}
 
-	var url = '/albumart';
+//	var url = '/albumart';
 
-	if (web != undefined)
-		url = url + web;
+//	if (web != undefined)
+//		url = url + web;
 
-	if (web != undefined && path != undefined)
-		url = url + '&';
-	else if (path != undefined)
-		url = url + '?';
+//	if (web != undefined && path != undefined)
+//		url = url + '&';
+//	else if (path != undefined)
+//		url = url + '?';
 
-	if (path != undefined)
-		url = url + 'path=' + nodetools.urlEncode(path);
+//	if (path != undefined)
+//		url = url + 'path=' + nodetools.urlEncode(path);
 
-	return url;
-};
-
-
+//	return url;
+//};
 
 
 
-studyComp.prototype.search = function (query) {
-	var self=this;
-	var defer=libQ.defer();
+
+
+//studyComp.prototype.search = function (query) {
+//	var self=this;
+//	var defer=libQ.defer();
 
 	// Mandatory, search. You can divide the search in sections using following functions
 
-	return defer.promise;
-};
+//	return defer.promise;
+//};
 
-studyComp.prototype._searchArtists = function (results) {
+//studyComp.prototype._searchArtists = function (results) {
 
-};
+//};
 
-studyComp.prototype._searchAlbums = function (results) {
+//studyComp.prototype._searchAlbums = function (results) {
 
-};
+//};
 
-studyComp.prototype._searchPlaylists = function (results) {
+//studyComp.prototype._searchPlaylists = function (results) {
 
 
-};
+//};
 
-studyComp.prototype._searchTracks = function (results) {
+//studyComp.prototype._searchTracks = function (results) {
 
-};
+//};
